6fcaf6efbafc7a5a79b498acb3aa855ec11f474e
Hatem Toumi <hatem.tmi@gmail.com>
2014-04-18 21:01:47 +0100

    Initial commit

---
+++ src/com/test/displaybitmapsproject/activity/ImageAdapter.java
@@ -0,0 +1,150 @@
+package com.test.displaybitmapsproject.activity;
+
+import android.content.Context;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+import com.test.displaybitmapsproject.imagemanager.ImageFetcher;
+import com.test.displaybitmapsproject.imagemanager.RecyclingImageView;
+
+/**
+ * The main adapter that backs the GridView. This is fairly standard except the
+ * number of columns in the GridView is used to create a fake top row of empty
+ * views as we use a transparent ActionBar and don't want the real top row of
+ * images to start off covered by it.
+ */
+public class ImageAdapter extends BaseAdapter {
+
+	private final Context mContext;
+	private ImageFetcher mImageFetcher;
+
+	private int mItemHeight = 0;
+	private int mNumColumns = 0;
+	private int mActionBarHeight = 0;
+	private GridView.LayoutParams mImageViewLayoutParams;
+
+	public ImageAdapter(Context context, ImageFetcher imageFetcher) {
+		super();
+		mContext = context;
+		mImageFetcher = imageFetcher;
+
+		mImageViewLayoutParams = new GridView.LayoutParams(
+				GridView.LayoutParams.MATCH_PARENT,
+				GridView.LayoutParams.MATCH_PARENT);
+
+		// Calculate ActionBar height
+		TypedValue tv = new TypedValue();
+		if (context.getTheme().resolveAttribute(android.R.attr.actionBarSize,
+				tv, true)) {
+			mActionBarHeight = TypedValue.complexToDimensionPixelSize(tv.data,
+					context.getResources().getDisplayMetrics());
+		}
+	}
+
+	@Override
+	public int getCount() {
+		// If columns have yet to be determined, return no items
+		if (getNumColumns() == 0) {
+			return 0;
+		}
+
+		// Size + number of columns for top empty row
+		return Constants.imageThumbUrls.length + mNumColumns;
+	}
+
+	@Override
+	public Object getItem(int position) {
+		return position < mNumColumns ? null
+				: Constants.imageThumbUrls[position - mNumColumns];
+	}
+
+	@Override
+	public long getItemId(int position) {
+		return position < mNumColumns ? 0 : position - mNumColumns;
+	}
+
+	@Override
+	public int getViewTypeCount() {
+		// Two types of views, the normal ImageView and the top row of empty
+		// views
+		return 2;
+	}
+
+	@Override
+	public int getItemViewType(int position) {
+		return (position < mNumColumns) ? 1 : 0;
+	}
+
+	@Override
+	public boolean hasStableIds() {
+		return true;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup container) {
+		// BEGIN_INCLUDE(load_gridview_item)
+		// First check if this is the top row
+		if (position < mNumColumns) {
+			if (convertView == null) {
+				convertView = new View(mContext);
+			}
+			// Set empty view with height of ActionBar
+			convertView.setLayoutParams(new AbsListView.LayoutParams(
+					AbsListView.LayoutParams.MATCH_PARENT, mActionBarHeight));
+			return convertView;
+		}
+
+		// Now handle the main ImageView thumbnails
+		ImageView imageView;
+		if (convertView == null) { // if it's not recycled, instantiate and
+									// initialize
+			imageView = new RecyclingImageView(mContext);
+			imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+			imageView.setLayoutParams(mImageViewLayoutParams);
+		} else { // Otherwise re-use the converted view
+			imageView = (ImageView) convertView;
+		}
+
+		// Check the height matches our calculated column width
+		if (imageView.getLayoutParams().height != mItemHeight) {
+			imageView.setLayoutParams(mImageViewLayoutParams);
+		}
+
+		// Finally load the image asynchronously into the ImageView, this also
+		// takes care of
+		// setting a placeholder image while the background thread runs
+		mImageFetcher.loadImage(
+				Constants.imageThumbUrls[position - mNumColumns], imageView);
+		return imageView;
+		// END_INCLUDE(load_gridview_item)
+	}
+
+	/**
+	 * Sets the item height. Useful for when we know the column width so the
+	 * height can be set to match.
+	 * 
+	 * @param height
+	 */
+	public void setItemHeight(int height) {
+		if (height == mItemHeight) {
+			return;
+		}
+		mItemHeight = height;
+		mImageViewLayoutParams = new GridView.LayoutParams(
+				GridView.LayoutParams.MATCH_PARENT, mItemHeight);
+		notifyDataSetChanged();
+	}
+
+	public void setNumColumns(int numColumns) {
+		mNumColumns = numColumns;
+	}
+
+	public int getNumColumns() {
+		return mNumColumns;
+	}
+}
---
+++ src/com/test/displaybitmapsproject/activity/MainActivity.java
@@ -0,0 +1,144 @@
+package com.test.displaybitmapsproject.activity;
+
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.os.Build.VERSION_CODES;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.view.ViewTreeObserver;
+import android.widget.AbsListView;
+import android.widget.GridView;
+
+import com.test.displaybitmapsproject.R;
+import com.test.displaybitmapsproject.imagemanager.ImageCache;
+import com.test.displaybitmapsproject.imagemanager.ImageFetcher;
+
+public class MainActivity extends FragmentActivity {
+	private static final String TAG = "MainActivity";
+	private static final String IMAGE_CACHE_DIR = "thumbs";
+
+	private GridView mGridView;
+
+	private int mImageThumbSize;
+	private int mImageThumbSpacing;
+	private ImageAdapter mAdapter;
+	private ImageFetcher mImageFetcher;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		mImageThumbSize = getResources().getDimensionPixelSize(
+				R.dimen.image_thumbnail_size);
+		mImageThumbSpacing = getResources().getDimensionPixelSize(
+				R.dimen.image_thumbnail_spacing);
+
+		ImageCache.ImageCacheParams cacheParams = new ImageCache.ImageCacheParams(
+				getApplicationContext(), IMAGE_CACHE_DIR);
+
+		cacheParams.setMemCacheSizePercent(0.25f); // Set memory cache to 25% of
+													// app memory
+
+		// The ImageFetcher takes care of loading images into our ImageView
+		// children asynchronously
+		mImageFetcher = new ImageFetcher(getApplicationContext(),
+				mImageThumbSize);
+		mImageFetcher.setLoadingImage(R.drawable.ic_launcher);
+		mImageFetcher.addImageCache(getSupportFragmentManager(), cacheParams);
+		
+		// initialize views
+		initViews();
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		mImageFetcher.setExitTasksEarly(false);
+		mAdapter.notifyDataSetChanged();
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause();
+		mImageFetcher.setPauseWork(false);
+		mImageFetcher.setExitTasksEarly(true);
+		mImageFetcher.flushCache();
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		mImageFetcher.closeCache();
+	}
+
+	/**
+	 * Initialize views
+	 */
+	private void initViews()
+	{
+		mGridView = (GridView) findViewById(R.id.gridView);
+		mAdapter = new ImageAdapter(getApplicationContext(), mImageFetcher);
+
+		mGridView.setAdapter(mAdapter);
+		mGridView.setOnScrollListener(new AbsListView.OnScrollListener() {
+			@Override
+			public void onScrollStateChanged(AbsListView absListView,
+					int scrollState) {
+				// Pause fetcher to ensure smoother scrolling when flinging
+				if (scrollState == AbsListView.OnScrollListener.SCROLL_STATE_FLING) {
+					// Before Honeycomb pause image loading on scroll to help
+					// with performance
+					if (Build.VERSION.SDK_INT < VERSION_CODES.HONEYCOMB) {
+						mImageFetcher.setPauseWork(true);
+					}
+				} else {
+					mImageFetcher.setPauseWork(false);
+				}
+			}
+
+			@Override
+			public void onScroll(AbsListView absListView, int firstVisibleItem,
+					int visibleItemCount, int totalItemCount) {
+			}
+		});
+
+		// This listener is used to get the final width of the GridView and then
+		// calculate the
+		// number of columns and the width of each column. The width of each
+		// column is variable
+		// as the GridView has stretchMode=columnWidth. The column width is used
+		// to set the height
+		// of each view so we get nice square thumbnails.
+		mGridView.getViewTreeObserver().addOnGlobalLayoutListener(
+				new ViewTreeObserver.OnGlobalLayoutListener() {
+					@TargetApi(VERSION_CODES.JELLY_BEAN)
+					@Override
+					public void onGlobalLayout() {
+						if (mAdapter.getNumColumns() == 0) {
+							final int numColumns = (int) Math.floor(mGridView
+									.getWidth()
+									/ (mImageThumbSize + mImageThumbSpacing));
+							if (numColumns > 0) {
+								final int columnWidth = (mGridView.getWidth() / numColumns)
+										- mImageThumbSpacing;
+								mAdapter.setNumColumns(numColumns);
+								mAdapter.setItemHeight(columnWidth);
+
+								Log.d(TAG, "onCreateView - numColumns set to "
+										+ numColumns);
+
+								if (Build.VERSION.SDK_INT >= VERSION_CODES.JELLY_BEAN) {
+									mGridView.getViewTreeObserver()
+											.removeOnGlobalLayoutListener(this);
+								} else {
+									mGridView.getViewTreeObserver()
+											.removeGlobalOnLayoutListener(this);
+								}
+							}
+						}
+					}
+				});
+	}
+}
